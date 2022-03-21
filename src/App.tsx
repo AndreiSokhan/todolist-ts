5ef1ece184028c4c84b55c2f5d623c88b51aef82
Andrei <sokhanpageproofs@gmail.com>
2022-03-21 23:10:37 +0300

    restart_TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,15 @@
 import React, {useState} from 'react';
 import './App.css';
-import {TodoList} from "./components/TodoList";
-import {v1} from "uuid";
 
 
 export const App = () => {
 
     //useState - это hook  setTasks - команда которая говорит Reactу ПЕРЕРИСОВАТЬ
-    const [tasks, setTasks] = useState([
-        {id: v1(), title: "HTML&CSS", isDone: true},
-        {id: v1(), title: "JS", isDone: true},
-        {id: v1(), title: "ReactJS", isDone: false},
-        {id: v1(), title: "Rest API", isDone: true},
-        {id: v1(), title: "GraphQL", isDone: false}
-    ])
-
-    const removeTask = (newId: string) => {
-        // удалить строчку id которой приходит
-        let filtered = tasks.filter((el) => el.id !== newId)
-        setTasks(filtered)
-    }
-
-    let [valueButton, setValueButton] = useState('All')
-
-    const tasksFilter = (filterValue: string) => {
-        // filterValue == valueButton
-        setValueButton(filterValue)
-    }
-
-    const addTask = (newTitle:string) => {
-        let newTask = {id: v1(), title: newTitle, isDone: true}
-        setTasks([newTask, ...tasks])
-    }
-
-    let prokladka = tasks
-    if (valueButton === "Active") {
-        prokladka = tasks.filter(el => el.isDone === false)
-    }
-    if (valueButton === "Completed") {
-        prokladka = tasks.filter(el => el.isDone === true)
-    }
-
-    return (
-        <div className="App">
-            <TodoList
-                title={"What to learn"}
-                task={prokladka}
-                removeTask={removeTask}
-                tasksFilter={tasksFilter}
-                addTask={addTask}
-            />
-        </div>
-    );
+    const [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Rest API", isDone: true},
+        {id: 5, title: "GraphQL", isDone: false}
+    ]
 }
-
