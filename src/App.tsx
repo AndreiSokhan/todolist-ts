63d9065701a10b5d666ed4a249ab5d2ecd76f04b
Andrei <sokhanpageproofs@gmail.com>
2022-05-18 22:58:56 +0300

    todolist_restart_lesson_2

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,30 @@
 import React, {useState} from 'react';
 import './App.css';
-import {TodoList} from "./components/TodoList";
-import {v1} from "uuid";
-import {ModalWindow} from "./components/ModalWindow";
+import {Todolist} from "./components/Todolist";
 
-function App() {
+const App = () => {
 
-   let [tasks1, setTasks1] = useState([
-      {id: v1(), title: "HTML&CSS", isDone: true},
-      {id: v1(), title: "JS", isDone: true},
-      {id: v1(), title: "ReactJS", isDone: false},
-      {id: v1(), title: "RestAPI", isDone: false},
-      {id: v1(), title: "GraphQL", isDone: false}
+   let [tasks, setTasks] = useState([
+      {id: 1, title: "HTML&CSS", isDone: true},
+      {id: 2, title: "JS", isDone: true},
+      {id: 3, title: "React&JS", isDone: false},
+      {id: 4, title: "RestAPI", isDone: false},
+      {id: 5, title: "GraphQL", isDone: false}
    ])
 
-
-   const removeTask = (newId: string) => {
-      //удаляем строку id  которой мы получили
-      setTasks1(tasks1.filter(el => el.id !== newId))
+   const removeTask = (removeId:number) => {
+      setTasks(tasks.filter((el)=>el.id!==removeId))
    }
-
-   let [valueButton, setValueButton] = useState("All")
-
-   const tasksFilter = (filterValue: string) => {
-      setValueButton(filterValue)
-   }
-
-   const addTask = (newTitle: string) => {
-      let newTask = {id: v1(), title: newTitle, isDone: false}
-      setTasks1([newTask, ...tasks1])
-   }
-
-   let prokladka = tasks1
-   if (valueButton === "Active") {
-      prokladka = tasks1.filter(el => el.isDone === false)
-   }
-   if (valueButton === "Completed") {
-      prokladka = tasks1.filter(el => el.isDone === true)
-   }
-
 
    return (
       <div className="App">
-         {/*<ModalWindow name={"Window1"}>*/}
-         {/*   <>*/}
-         {/*      <input type="text"/>*/}
-         {/*      <input type="text"/>*/}
-         {/*      <input type="checkbox"/>*/}
-         {/*   </>*/}
-         {/*</ModalWindow>*/}
-         <TodoList
-            title={"What to learn"}
-            task={prokladka}
+         <Todolist
+            title={"What to lern"}
+            tasks={tasks}
             removeTask={removeTask}
-            tasksFilter={tasksFilter}
-            addTask={addTask}
          />
       </div>
    );
-}
+};
 
 export default App;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,91 +1,40 @@
-import React, {useState, KeyboardEvent, ChangeEvent} from "react";
-import {Button} from "./Button";
-import {Input} from "./Input";
+import React from 'react';
 
-type TasksPropsType = {
-   id: string,
-   title: string,
+type TasksPropsType={
+   id:number
+   title: string
    isDone: boolean
 }
 
-type TodoListPropsType = {
-   title: string,
-   task: Array<TasksPropsType>,
-   removeTask: (id: string) => void,
-   tasksFilter: (filterValue: string) => void,
-   addTask: (newTitle: string) => void
+type TodolistPropsType={
+   title:string
+   tasks:Array<TasksPropsType>
+   removeTask:(removeId:number)=>void
 }
 
-export const TodoList = (props: TodoListPropsType) => {
-
-   let [newTitle, setNewtitle] = useState('')
-
-   //
-   // const addTaskHandler = () => {
-   //    props.addTask(newTitle)
-   //    setNewtitle('')
-   // }
-   //
-   // const onKeyPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
-   //    if (event.key === 'Enter') {
-   //       return addTaskHandler()
-   //    }
-   // }
-   //
-   // const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-   //    setNewtitle(event.currentTarget.value)
-   // }
-
-   const FilterHandler = (filterValue: string) => {
-      props.tasksFilter(filterValue)
-   }
-   const removeTaskHandler = (elID: string) => {
-      props.removeTask(elID)
-   }
-   const addTaskHandler = () => {
-      props.addTask(newTitle)
-      setNewtitle('')
-   }
-
+export const Todolist = (props:TodolistPropsType) => {
    return (
-      <div>
-         <Input newTitle={newTitle} setNewtitle={setNewtitle} callBack={addTaskHandler}/>
-         <Button name={'+'} callBack={addTaskHandler}/>
-         {/*<FullInput callBack={props.addTask}/>*/}
          <div>
-            {/*<input value={newTitle}*/}
-            {/*       onKeyPress={onKeyPressHandler}*/}
-            {/*       onChange={onChangeHandler}*/}
-            {/*/>*/}
-            {/*/!*onClick не может в себе содержать функцию. При наличии функции мы всегда ее выносим вверх и в onClick оставляем только ссылку на нее*!/*/}
-            {/*<button onClick={addTaskHandler}>+</button>*/}
-            {/*<Button name={'+'} callBack={addTaskHandler}/>*/}
+            <h3>{props.title}</h3>
+            <div>
+               <input /><button>+</button>
+            </div>
+            <ul>
+               {props.tasks.map((el)=>{
+                  return(
+                     <li key={el.id}>
+                        <button onClick={()=>props.removeTask(el.id)}>x</button>
+                        <input type="checkbox" checked={el.isDone}/>
+                        <span>{el.title}</span>
+                     </li>
+                  )
+               })}
+            </ul>
+            <div>
+               <button>All</button>
+               <button>Active</button>
+               <button>Completed</button>
+            </div>
          </div>
-         <ul>
-            {props.task.map((el) => {
-               return (
-                  <li key={el.id}>
-                     {/*<button onClick={() => removeTaskHandler(el.id)}>x</button>*/}
-                     <Button name={'x'} callBack={() => removeTaskHandler(el.id)}/>
-                     <input type="checkbox" checked={el.isDone}/>
-                     <span>{el.title}</span>
-                  </li>
-               )
-            })}
-
-         </ul>
-         <div>
-            <Button name={'All'} callBack={() => FilterHandler('All')}/>
-            <Button name={'Active'} callBack={() => FilterHandler('Active')}/>
-            <Button name={'Completed'} callBack={() => FilterHandler('Completed')}/>
-         </div>
-      </div>
-   )
-}
-
-//метод массива map
-// Array.map((el)=>{
-//    return (
-//       el
-//    )
-// });
+   );
+};
